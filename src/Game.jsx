633a42e7b346119e6fbf646cc0974c536f5d0072
Nina Yao <nini88518@gmail.com>
2024-08-20 16:04:39 +0800

    feat: implement random word selection for the correct answer using custom hook

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,8 +1,9 @@
-import { useReducer } from "react";
+import { useState, useReducer } from "react";
 import Grid from "./Grid";
 import useInputHandler from "./useInputHandler";
+import useWordSelector from "./useWordSelector";
 
-const correctWord = "REACT";
+//const correctWord = "REACT";
 const initialState = {
   currentGuess: "",
   guesses: [],
@@ -28,7 +29,7 @@ const wordleReducer = (state, action) => {
 
     case "SUBMIT_GUESS":
       if (state.currentGuess.length === 5 && !state.isGameOver) {
-        const guessResult = checkGuess(state.currentGuess, correctWord);
+        const guessResult = checkGuess(state.currentGuess, action.correctWord);
         const newGuess = [...state.guesses, state.currentGuess];
         const newResults = [...state.results, guessResult];
         const isCorrect = guessResult.every((result) => result === "green");
@@ -70,31 +71,49 @@ const checkGuess = (guess, correctWord) => {
   guess.split("").forEach((letter, index) => {
     if (result[index] === "grey" && correctWordArray.includes(letter)) {
       result[index] = "yellow";
-      correctWordArray[correctWordArray.indexOf(letter)] = null; // 标记字母已使用
+      correctWordArray[correctWordArray.indexOf(letter)] = null;
     }
   });
+  console.log(correctWord);
 
   return result;
 };
 
 const Game = () => {
+  const [correctWord, setCorrectWord] = useState("");
   const [state, dispatch] = useReducer(wordleReducer, initialState);
-  useInputHandler(dispatch);
+
+  useWordSelector(setCorrectWord);
+
+  useInputHandler((action) => {
+    if (action.type === "SUBMIT_GUESS") {
+      dispatch({ type: "SUBMIT_GUESS", correctWord });
+    } else {
+      dispatch(action);
+    }
+  });
 
   return (
     <div className="flex flex-col items-center">
-      {[...Array(6)].map((_, index) => (
-        <Grid
-          key={index}
-          currentGuess={
-            index === state.currentRow
-              ? state.currentGuess
-              : state.guesses[index] || ""
-          }
-          result={state.results[index] || []}
-        />
-      ))}
-      {state.message && <p className="mt-4 text-lg">{state.message}</p>}
+      {correctWord && (
+        <>
+          {[...Array(6)].map((_, index) => (
+            <Grid
+              key={index}
+              currentGuess={
+                index === state.currentRow
+                  ? state.currentGuess
+                  : state.guesses[index] || ""
+              }
+              result={state.results[index] || []}
+            />
+          ))}
+
+          {state.message && (
+            <p className="mt-4 text-3xl font-bold">{state.message}</p>
+          )}
+        </>
+      )}
     </div>
   );
 };
